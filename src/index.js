e495f6f97098feb07c2690298d4acff7c9b36421
DialecticalLaw <d.shmuratkin@mail.ru>
2023-08-13 15:09:18 +0300

    feat: morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let morseSymbols = '';
+    for (let i = 0; i < expr.length; i += 2) {
+        if (i % 10 === 0 && i !== 0) {
+            morseSymbols = morseSymbols + '*';
+        }
+        if (expr[i] === '0' || expr[i] === '1') {
+            if (expr[i] === '1' && expr[i + 1] === '0') {
+                morseSymbols = morseSymbols + '.';
+            } else if (expr[i] === '1' && expr[i + 1] === '1') {
+                morseSymbols = morseSymbols + '-';
+            }
+        } else if (expr[i] === '*') {
+            morseSymbols = morseSymbols + 'SPACE*';
+            i = i + 10;
+        }
+    }
+    let arrMorse = morseSymbols.split('*');
+    for (let symb of arrMorse) {
+        if (symb === 'SPACE') {
+            result = result + ' ';
+        } else {
+            for (let i = 0; i < Object.keys(MORSE_TABLE).length; i++) {
+                if (symb === Object.keys(MORSE_TABLE)[i]) {
+                    result = result + MORSE_TABLE[Object.keys(MORSE_TABLE)[i]];
+                }
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = {
